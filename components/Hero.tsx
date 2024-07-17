0e3f643406b807e1cc0f2bc2c6bf0297c5a667b4
Viktor Rura <ruravictor@gmail.com>
2024-07-17 17:12:49 +0100

    Hero section is done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
@@ -40,6 +41,8 @@ const Hero = () => {
                     <a href="#about">
                         <MagicButton 
                             title="Book a class"
+                            icon={<FaLocationArrow />}
+                            position="right"
                         />
                     </a>
                 </div>
