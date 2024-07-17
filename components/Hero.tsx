147e9b133c1195470f79663692396970ef7da3a5
Viktor Rura <ruravictor@gmail.com>
2024-07-17 16:48:23 +0100

    Add text animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
     return (
@@ -17,10 +18,25 @@ const Hero = () => {
                     fill="blue"
                 />
             </div>
-            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
                 {/* Radial gradient for the container to give a faded look */}
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-                
+            </div>
+
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+                        Dynamic Web Magic with Next.js
+                    </p>
+                    <TextGenerateEffect
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                        words="One move is a step two moves are the dance"
+                    />
+
+                    <p className="text-center text-blue-100 md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                        Hi I&apos;m Iryna your dance coach!
+                    </p>
+                </div>
             </div>
         </div>
     );
