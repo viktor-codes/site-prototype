40a61eab90fbda7c9504d4e0541f1dc9ddfc95c3
Viktor Rura <ruravictor@gmail.com>
2024-07-17 20:03:23 +0100

    Add assets

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -10,7 +10,13 @@ const Grid = () => {
                         key={i}
                         title={item.title}
                         description={item.description}
-
+                        // remove icon prop
+                        // remove original classname condition
+                        className={item.className}
+                        img={item.img}
+                        imgClassName={item.imgClassName}
+                        titleClassName={item.titleClassName}
+                        spareImg={item.spareImg}
                     />
                 ))}
             </BentoGrid>
