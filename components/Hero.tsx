2025e94a688a2ac6b5923e036910d79074770aaf
Viktor Rura <ruravictor@gmail.com>
2024-07-17 17:08:42 +0100

    Add button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
@@ -26,7 +27,7 @@ const Hero = () => {
             <div className="flex justify-center relative my-20 z-10">
                 <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                     <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                        Dynamic Web Magic with Next.js
+                        Best dance classes in town
                     </p>
                     <TextGenerateEffect
                         className="text-center text-[40px] md:text-5xl lg:text-6xl"
@@ -36,6 +37,11 @@ const Hero = () => {
                     <p className="text-center text-blue-100 md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
                         Hi I&apos;m Iryna your dance coach!
                     </p>
+                    <a href="#about">
+                        <MagicButton 
+                            title="Book a class"
+                        />
+                    </a>
                 </div>
             </div>
         </div>
