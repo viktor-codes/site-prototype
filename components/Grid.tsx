77c23f2f032a0e3b102ed4b9b1631efdc5f00222
Viktor Rura <ruravictor@gmail.com>
2024-07-17 21:25:01 +0100

    Fix bento grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -3,22 +3,32 @@ import { gridItems } from "../data/index";
 const Grid = () => {
     return (
         <section id="about">
-            <BentoGrid className="w-full py-20">
-                {gridItems.map((item, i) => (
-                    <BentoGridItem
-                        id={item.id}
-                        key={i}
-                        title={item.title}
-                        description={item.description}
-                        // remove icon prop
-                        // remove original classname condition
-                        className={item.className}
-                        img={item.img}
-                        imgClassName={item.imgClassName}
-                        titleClassName={item.titleClassName}
-                        spareImg={item.spareImg}
-                    />
-                ))}
+            <BentoGrid>
+                {gridItems.map(
+                    ({
+                        id,
+                        title,
+                        description,
+                        img,
+                        className,
+                        imgClassName,
+                        titleClassName,
+                        spareImg,
+                    }) => (
+                        <BentoGridItem
+                            id={id}
+                            key={id}
+                            title={title}
+                            description={description}
+                            className={className}
+                            img={img}
+                            imgClassName={imgClassName}
+                            titleClassName={titleClassName}
+                            spareImg={spareImg}
+                        />
+                    )
+                )}
+                
             </BentoGrid>
         </section>
     );
