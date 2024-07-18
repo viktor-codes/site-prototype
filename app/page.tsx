b357fbacabc2aa5cb87a9c6ceefc276ab3d7b670
Viktor Rura <ruravictor@gmail.com>
2024-07-18 15:48:04 +0100

    Add canvas reveal component

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { FaHome } from "react-icons/fa";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
               <RecentProjects />
               <Clients />
               <Experience />
+              <Approach />
           </div>
       </main>
   );
