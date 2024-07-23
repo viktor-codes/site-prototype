c242be9bf5f92d1b12256bb0a655d9cafef3b369
Viktor Rura <ruravictor@gmail.com>
2024-07-23 23:37:03 +0100

    Try new formatter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,32 @@
-import { ThemeProvider } from "./provider";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from './provider'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-    title: "Viktor's Portfolio",
-    description: "My creative portfolio",
-};
+	title: "Viktor's Portfolio",
+	description: 'My creative portfolio',
+}
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	)
 }
